6e6ea3ba1ce085cc197bd82200c16f5a2672be56
EguJaredoBoyd <eguchidieberejared@gmail.com>
2025-09-15 23:51:55 +0100

    Completed MVC class requirements and implementations.

--- .history/server_20250915224956.js
+++ .history/server_20250915224956.js
@@ -11,7 +11,6 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
 
 
 /* ***********************
